e9fd99b2aa900b4663ae5b8a553c75c4c2f97930
mibrahim070 <mibrahim070@gmail.com>
2015-06-30 16:40:04 +0200

    Final GP

---
+++ src/test/java/com/fork/backgroundProcess/ForkLifecycleTest.java
@@ -0,0 +1,23 @@
+package com.fork.backgroundProcess;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class ForkLifecycleTest {
+
+	ForkLifecycle fork;
+
+	@Test
+	public void updateDevicesDataTest() {
+//		fork = new ForkLifecycle();
+//		List<String> devices = new ArrayList<String>();
+//		devices.add("device1");
+//		devices.add("device2");
+//		assertEquals(devices, fork.updateDevicesData());
+	}
+
+}
